71c122fb95aa7e310b1dda4acd1b8120e11c88da
v33n4 <vi.nigalli@hotmail.com>
2023-04-23 18:25:24 -0300

    teste-2

--- inc_cliente_modal.php
+++ inc_cliente_modal.php
@@ -4,20 +4,20 @@
 
 	$tabDepend = '<div class="container">';
 
-	//linha de Telefone
+	//linha de login
 	$tabDepend .= '<div  class="row">';
-        $tabDepend .= '<div  class="col-md-2"><label class="control-label">Telefone</label></div>';
-        $tabDepend .= '<div  class="col-md-4"><input type="text" class="form-control" id="txtTelefone" name="txtTelefone" size="10px";></div>';
+        $tabDepend .= '<div  class="col-md-2"><label class="control-label">Nome</label></div>';
+        $tabDepend .= '<div  class="col-md-4"><input type="text" class="form-control" id="txtNome" name="txtNome" size="10px";></div>';
     
-    //linha de Endereço
-        $tabDepend .= '<div class="col-md-2"><label class="control-label">Endereço</label></div>';
-        $tabDepend .= '<div class="col-md-4"><input type="password" class="form-control" id="txtEndereço" name="txtEndereço" size="10px;"></div>';
+    //linha de Senha
+        $tabDepend .= '<div class="col-md-2"><label class="control-label">Telefone</label></div>';
+        $tabDepend .= '<div class="col-md-4"><input type="text" class="form-control" id="txtTelefone" name="txtTelefone" size="10px;"></div>';
     $tabDepend .= '</div></br>';  
     
-	//linha Nome
+	//linha nome
     $tabDepend .= '<div  class="row">';
-		$tabDepend .= '<div  class="col-md-2"><label class="control-label">Nome</label></div>';
-		$tabDepend .= '<div  class="col-md-4"><input type="text" class="form-control" name="txtNome" id="txtNome" size="50px;"></div>';
+		$tabDepend .= '<div  class="col-md-2"><label class="control-label">Endereço</label></div>';
+		$tabDepend .= '<div  class="col-md-4"><input type="text" class="form-control" name="txtEndereço" id="txtEndereço" size="50px;"></div>';
 	$tabDepend .= '</div>';
 	echo $tabDepend;
 
@@ -25,5 +25,13 @@
 <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 <script>
-	 $('.telefone').mask('(00) 00000-0000');
+	 var SPMaskBehavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+},
+spOptions = {
+  onKeyPress: function(val, e, field, options) {
+      field.mask(SPMaskBehavior.apply({}, arguments), options);
+    }
+};
 
+$('#txtTelefone').mask(SPMaskBehavior, spOptions);
